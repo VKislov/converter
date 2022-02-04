ae08c3fc82c33d7f1681b43a34de50b78f07bd3c
Vkislov <wkislov0@gmail.com>
2022-02-04 20:36:06 +0500

    Prod 1.0.1

--- src/api/initialAPI.js
+++ src/api/initialAPI.js
@@ -1,10 +1,6 @@
-import axios from "axios";
+import api from "./userAPI";
 export const initialAPI = {
   initialExt: () => {
-    return axios({
-      method: "get",
-      url: "http://u1589542.plsk.regruhosting.ru/AvailableExtensions",
-      headers: { Authorization: `${localStorage.getItem("token")}` },
-    });
+    return api.get("AvailableExtensions");
   },
 };
