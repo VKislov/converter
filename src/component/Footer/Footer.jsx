5324992d149f33fdf3a78f0c424fa24a2e01a75a
Vkislov <wkislov0@gmail.com>
2022-02-09 13:55:33 +0500

    Prod 1.0.5

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,4 +1,4 @@
 import style from "./Footer.module.css";
 export const Footer = () => {
-  return <div className={style.footer}>Footer</div>;
+  return <div className={style.footer}>&#169;GachiBoyConverter</div>;
 };
